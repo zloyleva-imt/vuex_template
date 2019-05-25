7d68d06f10b7632f2504c7c24b5ecb19bf92971c
ZloyLeva <zloyleva@gmail.com>
2019-05-25 11:55:51 +0300

    create simple counter

--- src/main.js
+++ src/main.js
@@ -3,6 +3,22 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import Vuex from 'vuex';
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+	state: {
+		count: 0
+	},
+	mutations:{
+		setCount(state, x){
+			console.log("setCount");
+			state.count = state.count + x;
+		}
+	}
+})
+
 new Vue({
   render: h => h(App),
+  store,
 }).$mount('#app')
